a6da9c3fadd68f994d847f9d932ebf44da8504f5
Syed Hisham Akmal <hisham0502@gmail.com>
2023-12-05 00:03:24 +0530

    Add and Delete Clients Handlers created

--- backend/internal/handlers/client_handler.go
+++ backend/internal/handlers/client_handler.go
@@ -34,3 +34,32 @@ func GetClientByID(c *fiber.Ctx) error {
 
 	return c.Status(200).JSON(client)
 }
+
+
+func AddClient(c *fiber.Ctx) error {
+    client := new(models.Client) //returns a pointer
+
+    if err := c.BodyParser(client); err != nil {
+        return c.Status(503).SendString(err.Error())
+    }
+
+    configs.DB.Create(&client)
+    return c.Status(fiber.StatusCreated).JSON(client)
+}
+
+func 	DeleteClient(c *fiber.Ctx) error {
+
+	clientID := c.Params("id")
+
+	var existingClient models.Client
+	result := configs.DB.First(&existingClient, clientID)
+	if result.Error != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Client not found"})
+	}
+
+	// Delete the client from the database
+	configs.DB.Delete(&existingClient)
+
+	// Return a success message
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{"message": "Client deleted successfully"})
+}
